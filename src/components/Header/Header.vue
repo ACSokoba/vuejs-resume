201d4661728bac5619f1c0f85a53290db99a226e
aboubakar sokoba <sokobachristian@yahoo.fr>
2021-07-14 23:09:46 +0200

    added more infos

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -8,18 +8,23 @@
       <h1>Fullstack Developer</h1>
       <div class="infos">
         <p>
-        <a class="pr-2" href="mailto:sokobachristian@yahoo.fr">sokobachristian@yahoo.fr</a>
-        <span>&#8729;</span>
-        <a class="px-2" href="tel:+33781657490">+33 7 81 65 74 90</a>
-        <span>&#8729;</span> <span class="px-2">Paris</span></p>
+          <a class="pr-2" href="mailto:sokobachristian@yahoo.fr"
+            >sokobachristian@yahoo.fr</a>
+          <span>&#8729;</span>
+          <a class="px-2" href="tel:+33781657490">+33 7 81 65 74 90</a>
+          <span>&#8729;</span>
+          <a class="px-2" href="https://www.github.com/ACSokoba">Github</a>
+          <span>&#8729;</span>
+          <a class="px-2" href="https://www.linkedin.com/in/aboubakar-christian-sokoba-304418146?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BzVxjF4dNSjOainKVHPUjcA%3D%3D">Linkedin</a>
+        </p>
       </div>
     </div>
   </header>
   <div class="description">
     Hi, i'm a fullstack software engineer based in Paris. I'm mainly proficient
-    in frontend. I'm passionate about coding, new technologies and anything tech-related. This is a
-    minimalist personal page where i sum-up my experience, subjects i've worked
-    on and techno i've used.
+    in frontend. I'm passionate about coding, new technologies and anything
+    tech-related. This is a minimalist personal page where i sum up my
+    experience, subjects i've worked on and techno i've used.
   </div>
 </template>
 
