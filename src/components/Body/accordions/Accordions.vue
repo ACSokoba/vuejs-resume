03c6b7bb8e3d1b1b4d3bb473a3dbc46e21c4748f
ACSokoba <44870671+ACSokoba@users.noreply.github.com>
2022-04-16 04:16:10 +0200

    minor improvements

--- src/components/Body/accordions/Accordions.vue
+++ src/components/Body/accordions/Accordions.vue
@@ -1,112 +1,127 @@
 <template>
-  <!-- <div class="c">
-    <input type="checkbox" id="experience-section" checked class="section-input" />
-    <h3><label for="experience-section">Experience</label></h3>
-    <div class="p">
-      <p>
-        <b class="job-title">
-          <span style="margin-right: 30px">Fullstack developer at Orange</span>
-          <span>jan 2020 - today</span>
-        </b>
-        <br /><br />
-        <span>&#8226;</span> Fullstack developer on the digital customer area in
-        an agile SAFe framework:
-        <br />
-        <span>&#8226;</span> Development, mainly front-end, of a dozen of
-        customer journey on projects with important business value:
-        Implementation of business needs, integration of UI / UX templates.
-        Evolution on a modular Angular architecture: lazy-loading, one feature
-        team one front-end module type of organization, structured CI/CD etc
-        <br />
-        <span>&#8226;</span> Backend development in NodeJS on a REST
-        microservices architecture
-        <br />
-        <span>&#8226;</span> Code migration for Angular upgrades, bug fixes
-        Refactoring, improvement of existing code, writing and implementation of
-        unit tests and integration tests
-        <br />
-        <span>&#8226;</span> Helped with the onboarding of new interns /
-        developers in the team in particular by writing a starter-kit
-        <br /><br />
-        <b>Technical environment : </b> Angular 11, Nodejs 14, Javascript,
-        Typescript, Git, Html, CSS, Bootstrap, Jenkins, VsCode, Jest, Jasmine,
-        Mocha, Protractor, Jira, Con
-      </p>
-      <p>
-        <b class="job-title">
-          <span style="margin-right: 30px"
-            >Software developer internship at Orange</span
+  <div>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingOne">
+          <button
+            class="accordion-button collapsed custom-accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseOne"
+            aria-expanded="true"
+            aria-controls="flush-collapseOne"
           >
-          <span>apr 2019 - sept 2019</span>
-        </b>
-        <br /><br />
-        <span>&#8226;</span> Project to redesign a network data decoding
-        application (ASN.1)<br />
-        <span>&#8226;</span> Need analysis: functions of the existing
-        application, new functions to be implemented, new GUI. <br />
-        <span>&#8226;</span> Choice of MEAN Stack: MongoDB, Express, Angular,
-        NodeJS <br />
-        <span>&#8226;</span> UML modeling, class diagram <br />
-        <span>&#8226;</span> Implementation of the abstract factory design
-        pattern for reusability, durability and application flexibility <br />
-        <br />
-        <b>Technical environment : </b> Angular 8, Nodejs, Typescript, Git, Html
-        5, CSS Bootstrap 4, VsCode
-      </p>
-    </div>
-  </div>
-  <div class="c">
-    <input type="checkbox" id="education-section" checked class="section-input"/>
-    <h3><label for="education-section">Education</label></h3>
-    <div class="p">
-      <p>
-        <b class="job-title">
-          <span style="margin-right: 30px"
-            >Networks and Telecommunication Engineer at Institut
-            Galilée</span
+            <h3>Experience</h3>
+          </button>
+        </h2>
+        <div
+          id="flush-collapseOne"
+          class="accordion-collapse collapse show"
+          aria-labelledby="flush-headingOne"
+        >
+          <div class="accordion-body">
+            <div class="text-start mb-3">
+              <b class="job-title">
+                <span style="margin-right: 30px"
+                  >Fullstack developer at Orange</span
+                >
+                <span class="badge bg-primary" style="font-size: 14px"
+                  >jan 2020 - today</span
+                >
+              </b>
+              <br /><br />
+              <span>&#8226;</span> Fullstack developer on the digital customer
+              area in an agile SAFe framework:
+              <br />
+              <span>&#8226;</span> Development, mainly front-end, of a dozen of
+              customer journey on projects with important business value:
+              Implementation of business needs, integration of UI / UX
+              templates. Evolution on a modular Angular architecture:
+              lazy-loading, one feature team one front-end module type of
+              organization, structured CI/CD etc
+              <br />
+              <span>&#8226;</span> Backend development in NodeJS on a REST
+              microservices architecture
+              <br />
+              <span>&#8226;</span> Code migration for Angular upgrades, bug
+              fixes Refactoring, improvement of existing code, writing and
+              implementation of unit tests and integration tests
+              <br />
+              <span>&#8226;</span> Helped with the onboarding of new interns /
+              developers in the team in particular by writing a starter-kit
+              <br /><br />
+              <b>Technical environment : </b> Angular 11, Nodejs 14, Javascript,
+              Typescript, Git, Html, CSS, Bootstrap, Jenkins, VsCode, Jest,
+              Jasmine, Mocha, Protractor, Jira, Con
+            </div>
+            <div class="w-100 d-flex justify-content-center mb-3">
+              <hr class="w-50" />
+            </div>
+            <div class="text-start">
+              <b class="job-title">
+                <span style="margin-right: 30px"
+                  >Software developer internship at Orange</span
+                >
+                <span class="badge bg-primary" style="font-size: 14px"
+                  >apr 2019 - sept 2019</span
+                >
+              </b>
+              <br /><br />
+              <span>&#8226;</span> Project to redesign a network data decoding
+              application (ASN.1)<br />
+              <span>&#8226;</span> Need analysis: functions of the existing
+              application, new functions to be implemented, new GUI. <br />
+              <span>&#8226;</span> Choice of MEAN Stack: MongoDB, Express,
+              Angular, NodeJS <br />
+              <span>&#8226;</span> UML modeling, class diagram <br />
+              <span>&#8226;</span> Implementation of the abstract factory design
+              pattern for reusability, durability and application flexibility
+              <br />
+              <br />
+              <b>Technical environment : </b> Angular 8, Nodejs, Typescript,
+              Git, Html 5, CSS Bootstrap 4, VsCode
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingTwo">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseTwo"
+            aria-expanded="true"
+            aria-controls="flush-collapseTwo"
           >
-          <span>2016 - 2019</span>
-        </b>
-        <br /><br />
-        Major in Development of distributed applications
-        <br />
-      </p>
-    </div>
-  </div> -->
-<div>
-  <div class="accordion accordion-flush" id="accordionFlushExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+            <h3>Education</h3>
+          </button>
+        </h2>
+        <div
+          id="flush-collapseTwo"
+          class="accordion-collapse collapse show"
+          aria-labelledby="flush-headingTwo"
+        >
+          <div class="accordion-body">
+            <div class="text-start">
+              <b class="job-title">
+                <span style="margin-right: 30px"
+                  >Networks and Telecommunication Engineer at Institut
+                  Galilée</span
+                >
+                <span class="badge bg-primary" style="font-size: 14px"
+                  >2016 - 2019</span
+                >
+              </b>
+              <br /><br />
+              Major in Development of distributed applications
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-    </div>
-  </div>
-</div>
-</div>
 </template>
 
 <script src="./accordions.ts"></script>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -9,20 +9,88 @@
       <div class="infos">
         <p>
           <a style="padding-right: 8px" href="mailto:sokobachristian@yahoo.fr"
-            >sokobachristian@yahoo.fr</a>
-          <span>&#8729;</span>
-          <a class="px-2" href="tel:+33781657490">+33 7 81 65 74 90</a>
-          <span>&#8729;</span>
-          <a class="px-2" href="https://www.github.com/ACSokoba">Github</a>
-          <span>&#8729;</span>
-          <a class="px-2" href="https://www.linkedin.com/in/aboubakar-christian-sokoba-304418146?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BzVxjF4dNSjOainKVHPUjcA%3D%3D">Linkedin</a>
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              role="img"
+              class="iconify iconify--ic"
+              width="32"
+              height="32"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5l-8-5V6l8 5l8-5v2z"
+              ></path></svg
+          ></a>
+          <a class="px-2" href="tel:+33781657490"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              role="img"
+              class="iconify iconify--pepicons"
+              width="32"
+              height="32"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 20 20"
+            >
+              <g fill="currentColor">
+                <path
+                  fill-rule="evenodd"
+                  d="M14.5 0h-9a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-9 18V2h9v16h-9Z"
+                  clip-rule="evenodd"
+                ></path>
+                <path
+                  d="M10 17.25a1.25 1.25 0 1 1 0-2.5a1.25 1.25 0 0 1 0 2.5Z"
+                ></path>
+              </g></svg
+          ></a>
+          <a class="px-2" href="https://www.github.com/ACSokoba"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              role="img"
+              class="iconify iconify--bi"
+              width="32"
+              height="32"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill="currentColor"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59c.4.07.55-.17.55-.38c0-.19-.01-.82-.01-1.49c-2.01.37-2.53-.49-2.69-.94c-.09-.23-.48-.94-.82-1.13c-.28-.15-.68-.52-.01-.53c.63-.01 1.08.58 1.23.82c.72 1.21 1.87.87 2.33.66c.07-.52.28-.87.51-1.07c-1.78-.2-3.64-.89-3.64-3.95c0-.87.31-1.59.82-2.15c-.08-.2-.36-1.02.08-2.12c0 0 .67-.21 2.2.82c.64-.18 1.32-.27 2-.27c.68 0 1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82c.44 1.1.16 1.92.08 2.12c.51.56.82 1.27.82 2.15c0 3.07-1.87 3.75-3.65 3.95c.29.25.54.73.54 1.48c0 1.07-.01 1.93-.01 2.2c0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+              ></path></svg
+          ></a>
+          <a
+            class="px-2"
+            href="https://www.linkedin.com/in/aboubakar-christian-sokoba-304418146?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BzVxjF4dNSjOainKVHPUjcA%3D%3D"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true"
+              role="img"
+              class="iconify iconify--entypo-social"
+              width="32"
+              height="32"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fill="currentColor"
+                d="M10 .4C4.698.4.4 4.698.4 10s4.298 9.6 9.6 9.6s9.6-4.298 9.6-9.6S15.302.4 10 .4zM7.65 13.979H5.706V7.723H7.65v6.256zm-.984-7.024c-.614 0-1.011-.435-1.011-.973c0-.549.409-.971 1.036-.971s1.011.422 1.023.971c0 .538-.396.973-1.048.973zm8.084 7.024h-1.944v-3.467c0-.807-.282-1.355-.985-1.355c-.537 0-.856.371-.997.728c-.052.127-.065.307-.065.486v3.607H8.814v-4.26c0-.781-.025-1.434-.051-1.996h1.689l.089.869h.039c.256-.408.883-1.01 1.932-1.01c1.279 0 2.238.857 2.238 2.699v3.699z"
+              ></path></svg
+          ></a>
         </p>
       </div>
     </div>
   </header>
   <div class="description">
-    Hi, I am a fullstack software engineer based in Paris, mainly proficient
-    in frontend. I am passionate about coding, new technologies and anything
+    Hi, I am a fullstack software engineer based in Paris, mainly proficient in
+    frontend. I am passionate about coding, new technologies and anything
     tech-related. This is a minimalist personal page where i sum up my
     experience, subjects I have worked on and the tech stacks I used.
   </div>
